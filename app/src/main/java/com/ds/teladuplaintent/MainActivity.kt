d90ebd600d783981b4eab6e7bb42ffbc36ad7bf8
Vinicius Sampaio <vinicius_travessa@hotmail.com>
2022-06-01 22:20:09 -0300

    TelaDulpaCompleta

--- app/src/main/java/com/ds/teladuplaintent/MainActivity.kt
+++ app/src/main/java/com/ds/teladuplaintent/MainActivity.kt
@@ -2,10 +2,27 @@ package com.ds.teladuplaintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    /** Essa função é chamada sempre que o usuário toca no botão enviar */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+    // Do something in response to button
+
+
 }
